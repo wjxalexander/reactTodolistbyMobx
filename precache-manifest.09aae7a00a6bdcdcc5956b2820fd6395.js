8f5f3373937e1a830b5a95cab1415a092186353c
wjxalexander <wjxalexander@hotmail.com>
2019-04-17 20:15:26 +0800

    Updates

---
+++ precache-manifest.09aae7a00a6bdcdcc5956b2820fd6395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactTodolistbyMobx/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3b0f0ac31927fd6ecb4c",
+    "url": "/reactTodolistbyMobx/static/js/runtime~main.ba945203.js"
+  },
+  {
+    "revision": "75b319766e03beb97f84",
+    "url": "/reactTodolistbyMobx/static/js/main.7ee5af75.chunk.js"
+  },
+  {
+    "revision": "2c31fc19ae217efb2511",
+    "url": "/reactTodolistbyMobx/static/js/2.4fe68b64.chunk.js"
+  },
+  {
+    "revision": "75b319766e03beb97f84",
+    "url": "/reactTodolistbyMobx/static/css/main.09a3dc60.chunk.css"
+  },
+  {
+    "revision": "2c31fc19ae217efb2511",
+    "url": "/reactTodolistbyMobx/static/css/2.01b5b191.chunk.css"
+  },
+  {
+    "revision": "8e18ad41064bebd9cbaec754e4a3708f",
+    "url": "/reactTodolistbyMobx/index.html"
+  }
+];
